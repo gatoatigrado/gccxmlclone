1f749c07dfd30a1973c72e6a0f1810340c617ec0
king <king>
2002-03-29 16:38:30 +0000

    ERR: Conversion of output path should be done only for argv[0] on exec call.

--- GCC_XML/GXFront/gxFront.cxx
+++ GCC_XML/GXFront/gxFront.cxx
@@ -3,8 +3,8 @@
   Program:   GCC-XML
   Module:    $RCSfile: gxFront.cxx,v $
   Language:  C++
-  Date:      $Date: 2002-03-29 16:31:45 $
-  Version:   $Revision: 1.5 $
+  Date:      $Date: 2002-03-29 16:38:30 $
+  Version:   $Revision: 1.6 $
 
   Copyright (c) 2002 Insight Consortium. All rights reserved.
   See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
@@ -115,10 +115,6 @@ int main(int argc, char** argv)
   parser.Parse(cGCCXML_FLAGS.c_str());
   parser.Parse(cGCCXML_USER_FLAGS.c_str());
   
-  // Convert the program path to a platform-dependent format.
-  cGCCXML_EXECUTABLE =
-    gxSystemTools::ConvertToOutputPath(cGCCXML_EXECUTABLE.c_str());
-  
   // Create the set of flags.
   std::vector<std::string> flags;
   parser.AddParsedFlags(flags);
@@ -137,8 +133,13 @@ int main(int argc, char** argv)
       }
     }
   
+  // Convert the program path to a platform-dependent format.
+  std::string cge =
+    gxSystemTools::ConvertToOutputPath(cGCCXML_EXECUTABLE.c_str());
+  
+  // Prepare list of arguments for exec call.
   char** args = new char*[flags.size()+2];
-  args[0] = strdup(cGCCXML_EXECUTABLE.c_str());
+  args[0] = strdup(cge.c_str());
   for(unsigned int i=0; i < flags.size(); ++i)
     {
     args[i+1] = strdup(flags[i].c_str());
