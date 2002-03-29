082fc6fa0dc4a457f4262eb2540160cdf46f9f1e
king <king>
2002-03-29 14:11:16 +0000

    ENH: Usage is now printed when no arguments for the real GCC-XML executable are given.

--- GCC_XML/GXFront/gxFront.cxx
+++ GCC_XML/GXFront/gxFront.cxx
@@ -3,8 +3,8 @@
   Program:   GCC-XML
   Module:    $RCSfile: gxFront.cxx,v $
   Language:  C++
-  Date:      $Date: 2002-03-28 16:00:29 $
-  Version:   $Revision: 1.3 $
+  Date:      $Date: 2002-03-29 14:11:16 $
+  Version:   $Revision: 1.4 $
 
   Copyright (c) 2002 Insight Consortium. All rights reserved.
   See ITKCopyright.txt or http://www.itk.org/HTML/Copyright.htm for details.
@@ -49,16 +49,15 @@ void printUsage(std::ostream& os)
     "the real executable.  Configuration settings are described below.\n"
     "\n"
     "Options include:\n"
-    "  --help                 = Print this usage information and exit.\n"
-    "  --version              = Print the version line and exit.\n"
-    "  --print                = Print the configuration settings and exit.\n"
-    "  -gccxml-executable xxx = Use \"xxx\" as the real gccxml executable.\n"
-    "  -gccxml-compiler xxx   = Use \"xxx\" to find GCCXML_FLAGS setting.\n"
-    "  -gccxml-config xxx     = Read file \"xxx\" for configuration.\n"
-    "  -gccxml-root xxx       = Use directory \"xxx\" to find supprot library.\n"
+    "  --help                  = Print this usage information and exit.\n"
+    "  --version               = Print the version line and exit.\n"
+    "  --print                 = Print the configuration settings and exit.\n"
+    "  --gccxml-executable xxx = Use \"xxx\" as the real gccxml executable.\n"
+    "  --gccxml-compiler xxx   = Use \"xxx\" to find GCCXML_FLAGS setting.\n"
+    "  --gccxml-config xxx     = Read file \"xxx\" for configuration.\n"
+    "  --gccxml-root xxx       = Use directory \"xxx\" to find supprot library.\n"
     "\n"
-    "  Additional -I and -D compiler flags can also be given.\n"
-    "(incomplete documentation...)\n";
+    "  Additional -I and -D compiler flags can also be given.\n";
 }
 
 int main(int argc, char** argv)
@@ -102,6 +101,7 @@ int main(int argc, char** argv)
     {
     std::cout
       << "No arguments given for real GCC-XML executable.  Not running it.\n";
+    printUsage(std::cout);
     return 0;
     }
   
